f689ea39d5f6441e340f1444b225b297a2b5df2b
julien D <lamihck@gmail.com>
2021-01-17 23:08:17 +0100

    css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./styles.scss";
 import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ViewPort } from "react-spaces";
+import { Fill, Layer, ViewPort } from "react-spaces";
 import Col from "./Col";
 import FirstCol from "./FirstCol";
 import LastCol from "./LastCol";
@@ -10,17 +10,25 @@ import LastCol from "./LastCol";
 export default function App() {
   let [players, setPlayers] = useState(Array(2).fill({name: "", score: 0}));
   return (
-    <div className="App">
-      <ViewPort className="papers">
-        <FirstCol />
-        {players.map((n, i) => (
-          <Col key={i}
-            setScore={score => setPlayers(players.map((p, pi) => pi === i ? {...p, score: score} : p))} 
-            setName={name => setPlayers(players.map((p, pi) => pi === i ? {...p, name: name} : p))}
-            isWinner={players[i].score === Math.max(...players.map(p => p.score))}
-          />
-        ))}
-        <LastCol onClick={() => setPlayers([...players, {name: "", score: 0}])}/>
+    <div className="App" >
+      <ViewPort>
+        <Layer zIndex={-1}>
+          <Fill style={{
+            backgroundColor: "#85625a",
+            backgroundImage: 'url("https://www.transparenttextures.com/patterns/wood-pattern.png")',
+          }}/>
+        </Layer>
+        <Fill  className="papers">
+          <FirstCol />
+          {players.map((n, i) => (
+            <Col key={i}
+              setScore={score => setPlayers(players.map((p, pi) => pi === i ? {...p, score: score} : p))} 
+              setName={name => setPlayers(players.map((p, pi) => pi === i ? {...p, name: name} : p))}
+              isWinner={players[i].score === Math.max(...players.map(p => p.score))}
+            />
+          ))}
+          <LastCol onClick={() => setPlayers([...players, {name: "", score: 0}])}/>
+        </Fill>
       </ViewPort>
     </div>
   );
--- src/LastCol.js
+++ src/LastCol.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { Left, Fill, CenterType } from "react-spaces";
+import { Left, Fill, CenterType, Right, Bottom } from "react-spaces";
 import Cell from "./Cell";
 import { Divider } from 'antd';
 
 export default function FirstCol(props) {
   return (
+    <Fill>
       <Fill onClick={props.onClick}>
         <Left size="40px" className="addColumn" centerContent={CenterType.HorizontalVertical}>
             <span style={{fontFamily: "arial"}}>+</span>
             <div/>
         </Left>
         <Fill></Fill>
+      </Fill>
+      <Right size="80px">
+        <Bottom size="80px" style={{
+          border: '1px solid #ccc8',
+          transform: 'rotate(5deg) translate(15px,15px)',
+          background: "linear-gradient(313deg, rgb(255, 255, 255) 27%, rgba(124, 124, 124, 0.184) 59%, rgba(255, 255, 255, 0.604) 54%, rgb(255, 255, 255) 100%)",
+          boxShadow: "inset 0 0 15px rgba(0,0,0,0.1), 1px 1px 3px rgba(0,0,0,0.2)",
+        }}
+        onClick={() => window.location.reload(false)}/>
+      </Right>
     </Fill>
   );
 }
